6bac6377c0091c9da6f4c961371b8ea1e1472c61
Shamsiddin-it <samsiddinarbobov@gmail.com>
2024-08-27 06:16:10 +0500

    zadzcha2

--- potato.py
+++ potato.py
@@ -1,12 +1,23 @@
-def potato(a:str):
-    cnt=0
-    i=0
-    while i<=len(a)-6:
-        if a[i:i+6]=="potato":
-            cnt+=1
-            i+=6
-        else:
-            i+=1 
-    print(cnt)
-a=input()
-potato(a)
+# def potato(a:str):
+#     cnt=0
+#     i=0
+#     while i<=len(a)-6:
+#         if a[i:i+6]=="potato":
+#             cnt+=1
+#             i+=6
+#         else:
+#             i+=1 
+#     print(cnt)
+# a=input()
+# potato(a)
+
+
+def sum_five(a):
+    a=[int(i) for i in a]
+    sum=0
+    for i in a:
+        if i>5:
+            sum+=i
+    print(sum)
+x=input().split()
+sum_five(x)
